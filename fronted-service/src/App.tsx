b27b97291269f51cd6be0995bab20968f791f38d
Aryya Bagus Padmanawijaya <aryyawijaya@gmail.com>
2024-05-31 15:45:19 +0700

    feat: add frontend service & add setup static ip

---
+++ fronted-service/src/App.tsx
@@ -0,0 +1,32 @@
+import { useState } from 'react';
+import './App.css'
+
+function App() {
+  const [apiResponse, setApiResponse] = useState<string | null>(null);
+
+  const handleApiCall = async () => {
+    try {
+      const response = await fetch('http://192.168.1.5'); // Replace with your actual API URL
+      if (response.ok) {
+        const data = await response.json();
+        setApiResponse(JSON.stringify(data, null, 2)); // Display the response as a formatted JSON string
+      } else {
+        setApiResponse('Error fetching data from the API');
+      }
+    } catch (error) {
+      console.log(error)
+      setApiResponse('An error occurred while fetching data');
+    }
+  };
+
+  return (
+    <div>
+      <button onClick={handleApiCall}>Call API</button>
+      {apiResponse && (
+        <pre>{apiResponse}</pre>
+      )}
+    </div>
+  );
+}
+
+export default App
--- startup-lb-server.sh
+++ startup-lb-server.sh
@@ -1,5 +1,44 @@
 #!/bin/bash
 
+# set static IP lb server
+if [[ -z "$1" ]]; then
+	echo "must set ip address"
+	exit 1
+fi
+if [[ -z "$2" ]]; then
+	echo "must set gateway"
+	exit 1
+fi
+
+IP_WITH_PREFIX="$1"
+IP=$(echo "$IP_WITH_PREFIX" | cut -d '/' -f 1)
+PREFIX_LENGTH=$(echo "$IP_WITH_PREFIX" | cut -d '/' -f 2)
+
+if ! [[ "$PREFIX_LENGTH" =~ ^[0-9]+$ ]]; then
+    echo "Invalid prefix length: $PREFIX_LENGTH"
+    exit 1
+fi
+
+sudo sh -c "cat << EOF > /etc/netplan/01-netcfg.yaml
+network:
+  version: 2
+  renderer: networkd
+  ethernets:
+    enp0s3:
+      dhcp4: no
+      addresses: [$IP/$PREFIX_LENGTH]
+      gateway4: $2
+      nameservers:
+        addresses: [8.8.8.8, 8.8.4.4]
+EOF"
+
+sudo netplan apply
+
+if [[ $? -ne 0 ]]; then
+	echo "failed to set static IP"
+	exit 1
+fi
+
 # update OS
 sudo apt -y update
 
